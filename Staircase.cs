cff11e102623c00c08a19cf4bb1dea88f9adab27
Archana <g.archana44@gmail.com>
2018-11-17 08:48:14 +0530

    Adding my All files upto 5 Programs

---
+++ Staircase.cs
@@ -0,0 +1,78 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the staircase function below.
+    static void staircase(int n) {
+        
+        for(int i=0;i<=n;i++)
+            {
+                for(int j=0;j<i;j++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine("");
+               
+               
+            }
+                          Console.WriteLine("");
+                Console.WriteLine("");
+
+            
+        for(int i=0;i<=n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+            Console.Write("#");
+           
+            }
+             Console.WriteLine(" ");
+        }
+        Console.WriteLine("");
+                Console.WriteLine("");
+
+        for(int i=0;i<=n;i++)
+        {
+            for(int j=i;j<n;j++)
+            {
+            Console.Write("#");
+           
+            }
+             Console.WriteLine(" ");
+        }
+
+          Console.WriteLine("");
+                Console.WriteLine("");
+
+       
+       for(int i=1;i<n;i++)
+       {
+           
+
+           for(int j=1;j<=i;j++)
+           {
+               Console.WriteLine("#");
+           }
+           Console.WriteLine(" ");
+       }
+    }
+
+
+    static void Main(string[] args) {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        staircase(n);
+    }
+}
